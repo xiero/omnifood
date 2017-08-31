f1e9482c2c38c79f0e515a4dc7647601e06855ef
H_Csaba <fxbitbucket@gmail.com>
2017-08-31 16:51:34 +0200

    animation for sections

--- resorces/js/scripts.js
+++ resorces/js/scripts.js
@@ -9,6 +9,7 @@ $(document).ready(function() {
 	  }, {
 			offset: '60px;'
 	});
+		
 	
 /*-------------scroll on buttons---------------------*/
 	
@@ -30,15 +31,37 @@ $(document).ready(function() {
 		  var target = $(this.hash);
 		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
 		  if (target.length) {
-			$('html, body').animate({
-			  scrollTop: target.offset().top
-			}, 1000);
+			$('html, body').animate({scrollTop: target.offset().top}, 1000);
 			return false;
 		  }
 		}
 	  });
 	});
+	
+/*--------------------animation on scroll-------------------------------*/
+	$('.js--wp-1').waypoint(function(direction){
+		$('.js--wp-1').addClass('animated fadeIn');
+	}, {
+		offset: '50%;'
+	});
   
+	$('.js--wp-2').waypoint(function(direction){
+		$('.js--wp-2').addClass('animated fadeInUp');
+	}, {
+		offset: '50%;'
+	});
+	
+	$('.js--wp-3').waypoint(function(direction){
+		$('.js--wp-3').addClass('animated fadeIn');
+	}, {
+		offset: '50%;'
+	});
+	
+	$('.js--wp-4').waypoint(function(direction){
+		$('.js--wp-4').addClass('animated pulse');
+	}, {
+		offset: '50%;'
+	});
     
     
 });
