f8758afa3bd5bc0ceebcb069b728a8572275b150
H_Csaba <fxbitbucket@gmail.com>
2017-09-01 16:21:01 +0200

    querie css for responsive mobile view, and hamburger icon beginn

--- resorces/js/scripts.js
+++ resorces/js/scripts.js
@@ -39,6 +39,7 @@ $(document).ready(function() {
 	});
 	
 /*--------------------animation on scroll-------------------------------*/
+	
 	$('.js--wp-1').waypoint(function(direction){
 		$('.js--wp-1').addClass('animated fadeIn');
 	}, {
