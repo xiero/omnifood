c489404b8ae192417900730b8809be012f3cdd73
H_Csaba <fxbitbucket@gmail.com>
2017-09-06 10:47:01 +0200

    responsive navigation bar with hambirger icon

--- resorces/js/scripts.js
+++ resorces/js/scripts.js
@@ -66,3 +66,28 @@ $(document).ready(function() {
     
     
 });
+
+
+/*------------------Mobile navigation /what will happen when the hamburger icon is clicked/------*/
+
+
+$('.js--nav-icon').click(function(){
+	
+	var nav = $('.js--main-nav');
+	var icon = $('.js--nav-icon i');
+	
+	nav.slideToggle(200);
+	
+	if (icon.hasClass('ion-navicon-round')) {
+		
+		icon.addClass('ion-close-round');
+		icon.removeClass('ion-navicon-round');
+		
+	} else {
+		
+		icon.addClass('ion-navicon-round');
+		icon.removeClass('ion-close-round');
+		
+	}
+	
+});
